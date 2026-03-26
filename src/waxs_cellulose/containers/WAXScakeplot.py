efad113c51ea65b1d660b237811d2f4d3ac042a2
Patrik Ahvenainen <patrik.ahvenainen@gmail.com>
2026-03-26 21:18:19 +0200

    feat: WAXScakeplot loading (csv)

--- src/waxs_cellulose/containers/WAXScakeplot.py
+++ src/waxs_cellulose/containers/WAXScakeplot.py
@@ -5,10 +5,33 @@ class WAXScakeplot:
     Preprocessed 2D scattering data presented as a function of 
     scattering vector length q and azimuthal angle (azim)
     Wherever possible, operations on intensities should not consider
-    masked values
+    masked values.
     """
     q: np.ndarray
     azim: np.ndarray
     I: np.ndarray
     mask: np.ndarray
     metadata: dict
+    
+    def __init__(self, intensities, azim, q, protected = False):
+        self.I = intensities
+        shape = self.I.shape
+        
+        if len(azim) == 2:
+            self.azim = np.linspace(azim[0], azim[1], shape[0])
+        elif len(azim) == shape[0]:
+            self.azim = azim
+        else:
+            raise ValueError("len(azim) was " + str(len(azim)) + 
+                             ", allowed values 2 and " + str(shape[0]))   
+            
+        if len(q) == 2:
+            self.q = np.linspace(q[0], q[1], shape[1])
+        elif len(q) == shape[1]:
+            self.q = q
+        else:
+            raise ValueError("len(q) was " + str(len(q)) + 
+                             ", allowed values 2 and " + str(shape[1]))
+         
+        self.mask = np.ones(shape)
+        self.protected = protected
--- src/waxs_cellulose/io/loaders.py
+++ src/waxs_cellulose/io/loaders.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 from waxs_cellulose.containers.WAXSpattern import WAXSpattern
-
+from waxs_cellulose.containers.WAXScakeplot import WAXScakeplot
 
 def load_intensities(filepath: str) -> np.ndarray:
     """Load simple two-column WAXS data (q, intensity)."""
@@ -38,3 +38,40 @@ def load_pattern(filepath: str, fileformat: str = "None", protected: bool = Fals
     
     return None
     
+def load_cakeplot(filepath: str, azim: np.ndarray, q: np.ndarray, 
+                  fileformat: str = "None", protected: bool = False):
+    """
+    Tries to open a 2D cake plot
+
+    Parameters
+    ----------
+    filepath : str
+        Path where the cakeplot (intensity as a funcion of q and azim) is.
+    azim : np.ndarray
+        Gives the azim-values for the cakeplot
+        length of array must be 2 or must match first dimension of cakeplot
+    q : np.ndarray
+        Gives the q-values for the cakeplot
+        length of array must be 2 or must match second dimension of cakeplot
+    fileformat : str
+        Format of the 2D scattering pattern file.
+        Supported formats: csv, image formats
+    protected : bool
+        Can be used to keep data unaltered
+
+    Returns
+    -------
+    WAXScakeplot if reading is succesful or None if unsuccesful.
+
+    """
+    
+    # If file format is not given try to guess it from file extension
+    if fileformat == "None":
+        filename, fileformat = os.path.splitext(filepath)
+    
+    if fileformat in ["csv", ".csv"]:
+        pattern = pd.read_csv(filepath, header=None).to_numpy()        
+        return WAXScakeplot(intensities=pattern, q=q, azim=azim, 
+                           protected=protected)
+    
+    return None
